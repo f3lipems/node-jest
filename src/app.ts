366b79bd4388c2dd6ad687ff8802f95bc67030c8
Felipe <felipe@visionar.io>
2024-03-31 11:12:46 -0300

    Add config and routes in express

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,28 @@
-import express, { Aplication } from 'express'
+import express from 'express'
 
 class App {
-    public app: Aplication
+    private app: express.Application
 
     constructor() {
         this.app = express()
+        this.config()
+        this.routes()
+    }
+
+    config() {
+        this.app.use(express.json())
+        this.app.use(express.urlencoded({ extended: true }))
+    }
+
+    routes() {
+        this.app.use('/', (res: any) => {
+            return res.json({ ok: true })
+        })
     }
 
     listen(port: number) {
         this.app.listen(port, () => {
             console.log('Server is running...')
-            
         })
     }
 }
